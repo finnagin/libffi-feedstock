b7dfc3d16e828072f8a5a66881cd1d002be84563
Finn Womack <flan313@gmail.com>
2024-09-22 21:10:57 -0700

    copy other files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
 if "%ARCH%"=="arm64" (
   copy C:\cpython-bin-deps\arm64\libffi-8.lib %LIBRARY_LIB%\libffi.lib
+  copy C:\cpython-bin-deps\arm64\libffi-8.dll %LIBRARY_LIB%\..\bin\ffi-8.dll
+  copy C:\cpython-bin-deps\arm64\include\ffi.h %LIBRARY_LIB%\..\include\ffi.h
+  copy C:\cpython-bin-deps\arm64\include\ffitarget.h %LIBRARY_LIB%\..\include\ffitarget.h
 ) else (
   call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 )
