9dea132c1dc3518da348c11616b36f981e9a4bd8
Finn Womack <flan313@gmail.com>
2024-09-22 21:04:57 -0700

    fix copy

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 if "%ARCH%"=="arm64" (
-  copy C:\cpython-bin-deps\arm64\libffi-8.lib libffi-8.lib
+  copy C:\cpython-bin-deps\arm64\libffi-8.lib %LIBRARY_LIB%\libffi.lib
 ) else (
   call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 )
 if %ERRORLEVEL% neq 0 exit 1
-copy libffi-8.lib %LIBRARY_LIB%\ffi.lib
+copy %LIBRARY_LIB%\libffi.lib %LIBRARY_LIB%\ffi.lib
